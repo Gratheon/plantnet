958d71fec0ff7371a6ca167542ed1c3af713298c
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-08-24 02:24:28 +0300

    add autodeploy

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 sudo -H -u www bash -c 'cd /www/plantnet/ && npm i' 
 cd /www/plantnet/
-docker-compose down
-COMPOSE_PROJECT_NAME=gratheon docker-compose up -d
+COMPOSE_PROJECT_NAME=gratheon docker-compose down
+COMPOSE_PROJECT_NAME=gratheon docker-compose up --build -d
